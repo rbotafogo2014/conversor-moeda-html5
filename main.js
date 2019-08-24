a1386ddbdae7a42c36e7f8be89e8ca4580f970e1
Rafael Botafogo <rbotafogo@gmail.com>
2019-08-24 01:29:12 +0000

    Aula 3 - Remoção das funções globais

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-function chamarAPI(){
-	console.log("Chamando API");
-	const endpoint = "api/latest";
-	var oParametros = { 
-		base: $("#moeda_origem").val()
-	};
-	$.get(endpoint, oParametros, callback );
-}
+const ovly = {  };
 
-function callback(resultado){
-	console.log(resultado);
-	var sMoedaDestino = $("#moeda_destino").val();
-	var fvalor = resultado.rates[sMoedaDestino];
-	$("#valor_destino").text(fvalor);
-	$("#moeda_destino_celula").text(sMoedaDestino);
-}
+ovly.conversor = { 
+	versao : "1" ,
+	chamarAPI: function(){
+		console.log("Chamando API");
+		const endpoint = "api/latest";
+		var oParametros = { 
+			base: $("#moeda_origem").val()
+		};
+		$.get(endpoint, oParametros, this.callback );
+	},
+	callback: function(resultado){
+		console.log(resultado);
+		var sMoedaDestino = $("#moeda_destino").val();
+		var fvalor = resultado.rates[sMoedaDestino];
+		$("#valor_destino").text(fvalor);
+		$("#moeda_destino_celula").text(sMoedaDestino);
+	}
+}	
+
+// function chamarAPI(){
+// 	console.log("Chamando API");
+// 	const endpoint = "api/latest";
+// 	var oParametros = { 
+// 		base: $("#moeda_origem").val()
+// 	};
+// 	$.get(endpoint, oParametros, callback );
+// }
+
+// function callback(resultado){
+// 	console.log(resultado);
+// 	var sMoedaDestino = $("#moeda_destino").val();
+// 	var fvalor = resultado.rates[sMoedaDestino];
+// 	$("#valor_destino").text(fvalor);
+// 	$("#moeda_destino_celula").text(sMoedaDestino);
+// }
 
